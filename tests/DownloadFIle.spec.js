95dfcd1398bf78e92de21ee6084936e81b718c48
mwislamqa <mwislamqa@gmail.com>
2025-09-23 10:16:44 -0400

    gitaction

--- tests/DownloadFIle.spec.js
+++ tests/DownloadFIle.spec.js
@@ -1,16 +1,29 @@
 
 import { test, expect } from '@playwright/test';
 
-test('file download in playwright', async ({ page }) => {
+test('file download in playwright', async ({ page  }, testInfo) => {
 
-    await page.goto('https://www.fcbcc.com')
+    await page.goto('https://www.lambdatest.com/selenium-playground/generate-file-to-download-demo');
 
-    // Start waiting for download before clicking. Note no await.
+
+    await page.locator('#textbox').click();
+    await page.locator('#textbox').fill('download this file using playwright automation');
+    await page.waitForTimeout(3000)
+    await page.locator("#create").click({button: 'force'});
+  
     const downloadPromise = page.waitForEvent('download');
-    await page.getByText('Download file').click();
+    await page.getByRole('link', { name: 'Download' }).click();
     const download = await downloadPromise;
 
+    // Start waiting for download before clicking. Note no await.
+
+
     // Wait for the download process to complete and save the downloaded file somewhere.
-    await download.saveAs('/path/to/save/at/' + download.suggestedFilename());
+    const downloadPath = download.suggestedFilename();
+    download.saveAs(downloadPath);
+    console.log(await downloadPath)
+    // Expect the file to be downloaded successfully.
+    expect(await downloadPath).toBe('info.txt');
+    await testInfo.attach('downloaded-file', { path: downloadPath });
 
 })
